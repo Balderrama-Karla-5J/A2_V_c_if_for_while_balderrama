85b23a55f85a9f7d092f15041393c883f2fe6ab4
Balderrama-Karla-5J <a.22308051281149@cbtis128.edu.mx>
2024-09-06 09:57:22 -0600

    Add files via upload
    agregue ejemplos de cadena,variables e if

---
+++ miscadenas.py
@@ -0,0 +1,24 @@
+a = """
+Karla Balderrama
+Trabajo con cadenas """
+
+nombre = "Karla"
+print(nombre[0])
+print(nombre)
+
+      
+susnovias=["E","l","i"]  
+for chicas in susnovias:
+        print(chicas)
+
+print("Otro ejemplo")
+b = "Karla Balderrama"
+print(b[0:5])
+print(" a mayusculas")
+a = "Karla Balderrama"
+print(a.upper())
+print("Concatenar")
+a = "Maria"
+b = "Perez"
+c = a + " " + b
+print(c)
---
+++ mivariables.py
@@ -0,0 +1,19 @@
+import math
+
+edad=17  #17 se guarda en edad
+nombre="Balderrama Karla"
+
+print("tu edad es ", edad)
+print("te llamas", nombre)
+print("-- tipo de variables ---")
+x = str(7)    # str funcion 
+y = int(3.1415)    # int funcion 
+z=float(7)         # float funcion 
+print(x)
+print(y)
+print(z)
+
+print("---funciones matematicas ----")
+
+x = math.sqrt(64)
+print(x)
